83e5a462f635eb34558e9ea7657b155b82ae5d35
Manuel Silva <manuel.silvag1@gmail.com>
2021-07-27 12:09:09 -0500

    Proyecto principal finalizado

--- Game.py
+++ Game.py
@@ -15,6 +15,11 @@ class Game:
         [6, 7, 8]
     ]
 
+    def user_exit(self):
+        os.system('clear')
+        print('Good Bye!')
+        exit()
+
     def __init__(self):
         self.title = 'Mj2silva Tic-Tac-Toe'
         self.board = Board()
@@ -33,15 +38,20 @@ class Game:
 
     def print_turn(self):
         turn_message = 'Turn of the player {player_index}'.format(
-            player_index=self.turn)
+            player_index=self.turn + 1)
         print(turn_message)
 
     def check_move(self, move):
         valid_move = True
-        for player in self.players:
-            if (move in player.owned_blocks):
-                print(move, ' in ', player.owned_blocks)
-                valid_move = False
+        if move > 8 or move < 0:
+            valid_move = False
+            self.error = 'Invalid move, you should enter a number between 0 and 8'
+        else:
+            for player in self.players:
+                if (move in player.owned_blocks):
+                    print(move, ' in ', player.owned_blocks)
+                    self.error = 'Invalid move, the block is already taken'
+                    valid_move = False
         return valid_move
 
     def move_player(self, next_move):
@@ -52,7 +62,7 @@ class Game:
 
     def print_board(self):
         # os.system('clear')
-        print(self.title)
+        print(self.title, '\n')
         self.board.print_board()
         if (self.invalid_move):
             print(self.error)
@@ -63,19 +73,23 @@ class Game:
         self.game_ended = True
 
     def check_board_status(self):
-        for player in self.players:
-            for winner_position in Game.winner_positions:
-                is_winner = False
-                for position in winner_position:
-                    if not position in player.owned_blocks:
-                        is_winner = False
-                        break
-                    is_winner = True
-                if is_winner:
-                    self.set_winner(self.turn)
+        if len(self.players[0].owned_blocks) + len(self.players[1].owned_blocks) == 8:
+            self.game_ended = True
+        else:
+            for player in self.players:
+                for winner_position in Game.winner_positions:
+                    is_winner = False
+                    for position in winner_position:
+                        if not position in player.owned_blocks:
+                            is_winner = False
+                            break
+                        is_winner = True
+                    if is_winner:
+                        self.set_winner(self.turn)
 
     def start_next_turn(self):
         try:
+            os.system('clear')
             self.print_board()
             next_move_input = input(
                 'Enter the number of an empty position or x to exit: ')
@@ -86,8 +100,6 @@ class Game:
                 self.invalid_move = not self.check_move(next_move)
                 if (not self.invalid_move):
                     self.move_player(next_move)
-                else:
-                    self.error = 'Invalid move, the block is already taken'
         except:
             self.invalid_move = True
             self.error = 'Invalid move, you should enter a number between 0 and 8'
@@ -95,7 +107,10 @@ class Game:
     def print_game_ended(self):
         if (self.game_ended):
             if (self.winner >= 0):
-                print('Congrats player {winner}! You Win!'.format(winner=self.winner))
+                os.system('clear')
+                self.print_board()
+                print('Congrats player {winner}! You Win!'.format(
+                    winner=self.winner + 1))
             else:
                 print('There are no movements left. The game finish in a tie!')
 
@@ -103,4 +118,7 @@ class Game:
         while (not self.exit and not self.game_ended):
             self.start_next_turn()
 
-        self.print_game_ended()
+        if (self.exit):
+            self.user_exit()
+        else:
+            self.print_game_ended()
